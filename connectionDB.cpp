9de95c70d58be441045b682deda72ce7c6479ac0
Stephanie <ishnewalon@hotmail.com>
2021-09-12 15:45:50 -0400

    Ajout de fonction CreateTable

--- connectionDB.cpp
+++ connectionDB.cpp
@@ -2,11 +2,24 @@
 #include <string>
 #include <pqxx/pqxx>
 
+void createTable(pqxx::connection &C){
+	const char * sqlStatement = "CREATE TABLE IF NOT EXISTS utilisateur(" \
+				"EMPLOYEE_ID INT PRIMARY KEY 	NOT NULL," \
+				"NOM 		TEXT		NOT NULL," \
+				"PRENOM 	TEXT		NOT NULL," \
+				"MDP 		TEXT		NOT NULL);";
+	pqxx::work W(C);
+	W.exec(sqlStatement);
+	W.commit();
+	std::cout << "La table a ete cree" << std::endl;
+}
+
 int main(){
 	try {
 		pqxx::connection C("dbname = connecttest user = ishnewalon password = inf556 hostaddr = 127.0.0.1 port = 5432" );
 		if (C.is_open()){
 			std::cout << "La connexion a reussie" << std::endl;
+			createTable(C);
 		}
 		else {
 			std::cout << "La connexion a echouee" << std::endl;
