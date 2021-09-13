cdd993d018d22bd1e50f69a8fe5953527aacff08
Stephanie <ishnewalon@hotmail.com>
2021-09-13 10:27:12 -0400

    Ajout fonction listRecords (SELECT)

--- connectionDB.cpp
+++ connectionDB.cpp
@@ -29,28 +29,45 @@ void populateTable(pqxx::connection &C){
 
 void emptyTable(pqxx::connection &C){
 	const char * sqlStatement = "DELETE FROM utilisateur;";
+	
 	pqxx::work W(C);
 	W.exec(sqlStatement);
 	W.commit();
 	std::cout << "DELETE a foncitonne avec succes" << std::endl;
 }
 
+void listRecords(pqxx::connection &C){
+	const char * sqlStatement = "SELECT * FROM utilisateur;";
+	
+	pqxx::nontransaction N(C);
+	pqxx::result R(N.exec(sqlStatement));
+	for (pqxx::result::const_iterator iterator = R.begin(); iterator != R.end(); ++iterator){ //See what is best ++iterator or iterator++
+		std::cout << "ID d'employee: " << iterator[0].as<int>() << std::endl;
+		std::cout << "Nom: " << iterator[1].as<std::string>() << std::endl;
+		std::cout << "Prenom: " << iterator[2].as<std::string>() << std::endl;
+		std::cout << "Mot de passe: " << iterator[3].as<std::string>() << std::endl;
+	}
+	std::cout << "Fin des donnes" << std::endl;
+}
+
 int main(){
-	try {
-		pqxx::connection C("dbname = connecttest user = ishnewalon password = inf556 hostaddr = 127.0.0.1 port = 5432" );
+	try{
+		pqxx::connection C("dbname = connecttest user = ishnewalon password = inf556 hostaddr = 127.0.0.1 port = 5432");
 		if (C.is_open()){
 			std::cout << "La connexion a reussie" << std::endl;
 			createTable(C);
-			//populateTable(C);
+			populateTable(C);
+			listRecords(C);
 			emptyTable(C);
+			listRecords(C);
 		}
-		else {
+		else{
 			std::cout << "La connexion a echouee" << std::endl;
 			return 1;
 		}
 		C.disconnect();
 	}
-	catch(const std::exception &e) {
+	catch(const std::exception &e){
 		std::cerr << e.what() << std::endl;
 		return 1;
 	}
